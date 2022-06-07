ecfd99d92e8ae79caa42f97d34f2c22bcf481e2d
Kácsor Bálint <kacsorb93@gmail.com>
2022-06-07 14:37:37 +0200

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // NOTE: An endpoint to get ALL the users in the db
+        // returns a list of users (List is okay instead of IEnumerable but we don't need that many features now)
+        [HttpGet] 
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();            
+        }
+
+        // NOTE: An endpoint to get a SPECIFIC user in the db
+        // Route parameter added to reach a user with a specific id like api/users/[id]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
